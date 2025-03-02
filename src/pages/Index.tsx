c46ab593111094440b2aa0199a7987d4af9d1a23
Cess <67199471+cessbub@users.noreply.github.com>
2025-03-02 23:16:55 +0800

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -23,7 +23,7 @@ const Index = () => {
           <div className="flex flex-col md:flex-row justify-between items-center">
             <div className="flex items-center space-x-2 mb-4 md:mb-0">
               <img 
-                src="/icon.png" 
+                src="https://buddybetes.com/icon.png" 
                 alt="BuddyBetes Logo" 
                 className="h-8 w-8" 
               />
